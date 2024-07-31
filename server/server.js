973c8d6e4ff9340db8e9903fc5099c6fd39373cb
pattyboyy <134738449+pattyboyy@users.noreply.github.com>
2024-07-31 00:08:42 -0600

    update server

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const startApolloServer = async () => {
 
   db.once('open', () => {
     app.listen(PORT, () => {
-      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
   });

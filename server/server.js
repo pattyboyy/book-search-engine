c3a3350c8450ef0caf588af0e2e177396221c61d
pattyboyy <134738449+pattyboyy@users.noreply.github.com>
2024-07-30 21:54:39 -0600

    fixed server.js for render

--- server/server.js
+++ server/server.js
@@ -7,14 +7,13 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
